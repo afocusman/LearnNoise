aaa70fc9d94a722a69b7c42f7ce24506b261c39c
LeafRiver <s_zhang@whu.edu.cn>
2016-12-17 16:14:32 +0800

    add normalization of amplitude

--- src/matlab/deconvolve.m
+++ src/matlab/deconvolve.m
@@ -1,10 +1,10 @@
-function [cor_out, npow, freq] = deconvolve(cor, delta)
-
+%function [cor_out, npow, freq] = deconvolve(cor, delta)
+function [amp_cor, amp_smooth, amp_whiten, cor_out, npow, freq] = deconvolve(cor, delta)
 npts = length(cor);
 
 
 % gaussian window
-alpha = 700; % smaller, sigma larger
+alpha = 1000; % smaller, sigma larger
 
 % damp to handle divided by zero
 damp = 1e-10;
@@ -31,9 +31,11 @@ amp_whiten = amp_cor ./ (amp_smooth + damp);
 
 % fft --> ifft
 cor_whiten = fft(amp_whiten, npow);
-
 cor_out = 2 * real(cor_whiten(1 : (npts-1) / 2)) ./ npow;
 
+% normalize
+maximum = max(abs(cor_out));
+cor_out = cor_out / maximum;
 
 end
 
